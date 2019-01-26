af1f9a492cea6b7eee8f67680e887729e3271934
yuberalberto <yuberalberto@gmail.com>
2019-01-26 17:42:46 -0500

    initial commit

---
+++ precache-manifest.c92ad0b8cadb8b9e010264aabb79b8b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a12d8bf4b20688024e9",
+    "url": "/robofriends/static/css/main.12f28b85.chunk.css"
+  },
+  {
+    "revision": "8a12d8bf4b20688024e9",
+    "url": "/robofriends/static/js/main.8a12d8bf.chunk.js"
+  },
+  {
+    "revision": "426dde71cdfe5dc6b837",
+    "url": "/robofriends/static/css/1.89117b37.chunk.css"
+  },
+  {
+    "revision": "426dde71cdfe5dc6b837",
+    "url": "/robofriends/static/js/1.426dde71.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "25ca6619de2569812fdf683a42990dc8",
+    "url": "/robofriends/index.html"
+  }
+];
